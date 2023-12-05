1d4b7ffbed89ca47a409d537683c57cb6ddde346
avery30809 <avery2070527@gmail.com>
2023-12-06 02:46:41 +0800

    刪除註解

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let showAllChampionsBtn = document.getElementById("showAllChampions");
     
     showAllChampionsBtn.addEventListener("click", () => {
-        let result = getAllChampions();
+        getAllChampions();
     });
     
     function getAllChampions () {
@@ -14,21 +14,21 @@ document.addEventListener("DOMContentLoaded", () => {
         })
         .then(response => response.json())
         .then(data => {
-//            if (data.error === undefined) {
-//                let championInfoElement = document.getElementById("championInfo");
-//                // Clear previous content
-//                championInfoElement.innerHTML = "";
-//                // Display champion names
-//                data.forEach(championName => {
-//                    let championItem = document.createElement("div");
-//                    championItem.textContent = championName;
-//                    championInfoElement.appendChild(championItem);
-//                });
-//            }
-//            else {
-//                window.alert("Champions are lost?");
-//                window.location.href = "../../index.php";
-//            }
+            if (data.error === undefined) {
+                let championInfoElement = document.getElementById("championInfo");
+                // Clear previous content
+                championInfoElement.innerHTML = "";
+                // Display champion names
+                data.forEach(championName => {
+                    let championItem = document.createElement("div");
+                    championItem.textContent = championName;
+                    championInfoElement.appendChild(championItem);
+                });
+            }
+            else {
+                window.alert("Champions are lost?");
+                window.location.href = "../../index.php";
+            }
         })
         .catch(error => {
             console.error('Error fetching champion data:', error);
