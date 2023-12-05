12de3cdbff4cc007f96384e6bc3298a0f0786ae7
avery30809 <avery2070527@gmail.com>
2023-12-06 02:46:29 +0800

    修復尚未讀取Database.php

--- inc/bootstrap.php
+++ inc/bootstrap.php
@@ -3,8 +3,8 @@ define("PROJECT_ROOT_PATH", __DIR__ . "/../");
 // include the base controller file 
 require_once PROJECT_ROOT_PATH . "/Controller/Api/BaseController.php";
 // include the use model file 
-require_once PROJECT_ROOT_PATH . "/Model/ChampionModel.php";
 require_once PROJECT_ROOT_PATH . "/Model/Database.php";
+require_once PROJECT_ROOT_PATH . "/Model/ChampionModel.php";
 
 define('DB_HOST', 'localhost');
 define('DB_USERNAME', 'choco');
